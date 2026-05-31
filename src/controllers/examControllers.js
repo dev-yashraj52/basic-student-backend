1e7777e09a5b903dbfb257adf40fa0c52f5bd1a0
Yash Raj <dev.yashraj52@gmail.com>
2026-06-01 01:02:46 +0530

    add basic strcuture for examControllers.js

---
+++ src/controllers/examControllers.js
@@ -0,0 +1,31 @@
+const db = require("../config/db.js");
+
+const getExams = async (req, res) => {
+
+};
+
+const getExamsById = async (req, res) => {
+
+};
+const createNewExam = async (req, res) => {
+
+};
+
+const updateExamNameById = async (req, res) => {
+
+};
+const updateExamMarksbyId = async (req, res) => {
+
+};
+const deleteExamById = async (req, res) => {
+
+};
+
+module.exports = {
+    getExams,
+    getExamsById,
+    createNewExam,
+    updateExamNameById,
+    updateExamMarksbyId,
+    deleteExamById
+};
