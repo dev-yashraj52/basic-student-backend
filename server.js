aaaba16f413de6e74844caee2d3b4e0de793ece4
Yash Raj <dev.yashraj52@gmail.com>
2026-06-01 00:52:58 +0530

    add examRoutes path inside server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(studentRoutes);
+app.use(examRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on PORT ${process.env.PORT}`)
