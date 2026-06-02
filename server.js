b1811dd5477f9ebed35893443596f99ea759e2fe
Yash Raj <dev.yashraj52@gmail.com>
2026-06-02 23:33:33 +0530

    add cors path in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require('dotenv').config({ path: '.env' });
-
+const cors = require("cors");
 const studentRoutes = require("./src/routes/studentRoutes.js");
 const examRoutes = require("./src/routes/examRoutes.js");
 
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
 app.use(studentRoutes);
 app.use(examRoutes);
 
