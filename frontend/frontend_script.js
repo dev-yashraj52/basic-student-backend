2fe51dfbf4d27b0664fadca89169385891430ed5
Yash Raj <dev.yashraj52@gmail.com>
2026-06-03 22:11:50 +0530

    fix index.html name typos and add code for student tab button working inside frontend_script

---
+++ frontend/frontend_script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.toggle-btn');
+const boxes = document.querySelectorAll('.content-student');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const targetId = button.getAttribute('data-target');
+        const targetBox = document.getElementById(targetId);
+
+        const isAlreadyActive = button.classList.contains('active');
+
+        buttons.forEach(btn => btn.classList.remove('active'));
+        boxes.forEach(box => box.classList.add('hidden'));
+
+        if (!isAlreadyActive) {
+            button.classList.add('active');
+            targetBox.classList.remove('hidden');
+        }
+    })
+})
